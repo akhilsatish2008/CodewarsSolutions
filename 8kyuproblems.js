a3a5f1408124b861920d4cb5d9d2b9721f7d63f9
Akhil Satish <akhilsatish2008@gmail.com>
2022-04-25 14:30:50 -0700

    new problem solved

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -214,7 +214,27 @@ function sumMix(x){
      return x.reduce((prev, curr)=>Number(prev)+Number(curr),0  )
     
     }
-
+/*
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+*/
+function toCsvText(array) {
+      for( let i = 0;i<array.length;i++){
+        array[i]=array[i].join(',')
+      }
+                      
+      return array.join("\n")
+    }
 
 
 
