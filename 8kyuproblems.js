aa9c4679ea5b586bde874fa6bccafc5a5c9d6635
Akhil Satish <akhilsatish2008@gmail.com>
2022-05-17 11:07:53 -0700

    another problem soleved on codewars arrays

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -311,4 +311,15 @@ function firstNonConsecutive (arr) {
       
       return first !== undefined ? first : null;
     }
-    
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+ */
+
+function removeEveryOther(arr){
+      return arr.filter(function(elem, index) {
+        return index % 2 === 0;
+      });
+    }
