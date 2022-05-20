eef30a5f20b1ca05578fa7ac9c3ea952107efaba
Akhil Satish <akhilsatish2008@gmail.com>
2022-05-19 18:46:24 -0700

    another daily code wars solved

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -339,3 +339,20 @@ function divisibleBy(numbers, divisor){
       return newArr
     
     }
+
+
+/*Write a method, that will get an integer array as parameter and will process every number from this array.
+
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+ [4,3,9,7,2,1] -> [2,9,3,49,4,1]*/
+ function squareOrSquareRoot(array) {
+  // get array either use map to map thru the values and spit out new array 
+  
+ return array.map( x =>{
+   let sqrt = Math.sqrt(x)
+   return Number.isInteger(sqrt)? sqrt : x*x;
+ } )
+}
+
