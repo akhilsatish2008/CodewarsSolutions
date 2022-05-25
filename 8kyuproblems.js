f50c681ac45a5f4f66359cac64e8f4bde8d55e99
Akhil Satish <akhilsatish2008@gmail.com>
2022-05-25 15:48:21 -0700

    solved another 8 kyu

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -401,3 +401,31 @@ function filter_list(l) {
   // Return a new array with the strings filtered out
  return  l.filter(x=>typeof(x)=== "number" ) // used filter to filter the type of input that x is
 }
+/*Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0 */
+
+function howMuchILoveYou(nbPetals) {
+  //Using if else statments to check whether which number correspods to the following phrase
+if(nbPetals%6 == 1){
+  return ("I love you");
+}else if(nbPetals%6 == 2){
+  return ("a little");
+}else if(nbPetals%6 ==3){
+  return ("a lot");
+}else if(nbPetals%6 ==4){
+  return ("passionately");
+}else if(nbPetals%6 == 5){
+  return ("madly");
+}else{
+  return ("not at all");
+}
+}
