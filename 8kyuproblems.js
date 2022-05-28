f23016e86111da41a2b7310c7112b22935303134
Akhil Satish <akhilsatish2008@gmail.com>
2022-05-27 21:04:40 -0700

    added another solution for today that i solved

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -429,3 +429,12 @@ if(nbPetals%6 == 1){
   return ("not at all");
 }
 }
+/*For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'. */
+function well(x){
+  
+  let countOfGood = x.filter(x => x == 'good').length;    // filter out length of word good 
+  return countOfGood < 1 ? 'Fail!' :    
+         countOfGood < 3 ? 'Publish!' : 'I smell a series!';
+}
