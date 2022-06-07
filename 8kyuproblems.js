664e6891c101d97049aae26ad3e2d5ce3fd8bd9c
Akhil Satish <akhilsatish2008@gmail.com>
2022-06-06 22:35:29 -0700

    iterated through an array without using a loop

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -536,3 +536,14 @@ function mergeArrays(arr1, arr2) {
   let sortedArr3= arr3.sort((a,b)=>a-b) // sort arr
   return [...new Set(sortedArr3)]; // return with no duplicates
 }
+/**** No Loops Allowed ***
+
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series: */
+
+function check(a, x) {
+  return a.includes(x);
+}
