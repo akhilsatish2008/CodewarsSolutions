2bcd3e3e30b95ef4c17bcd3b05653b12550f66ef
Akhil Satish <akhilsatish2008@gmail.com>
2022-06-16 17:28:49 -0700

    Problem solved

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -623,3 +623,24 @@ You will be given an array of all the family members' ages, in any order. The ag
 function differenceInAges(ages){
   return [Math.min(...ages), Math.max(...ages), Math.max(...ages)-Math.min(...ages)];
 }
+/*Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+Example
+[2, 1, 10]  -->  9
+In descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).*/
+function sumOfDifferences(arr) {
+ 
+  return arr
+    .sort((a, b) => b - a)
+    .reduce((acc, curr, index, array) => {
+      const next = array[index + 1];
+      if (!isNaN(curr - next)) {
+        acc += curr - next;
+      }
+      return acc;
+    }, 0);
+}
