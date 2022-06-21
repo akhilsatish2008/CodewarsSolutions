edb71e56da5257514e37a06510935adefc32ab27
Akhil Satish <akhilsatish2008@gmail.com>
2022-06-20 23:59:30 -0700

    7 kyu problem

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -644,3 +644,16 @@ function sumOfDifferences(arr) {
       return acc;
     }, 0);
 }
+
+function disemvowel(str) {
+
+  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  let arr = str.split('')
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (!vowels.includes(arr[i])) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr.join('')
+}
