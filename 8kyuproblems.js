50560aea805d7a304d01fe4c232a6e36e3df0f92
Akhil Satish <akhilsatish2008@gmail.com>
2022-06-21 23:54:38 -0700

    another problem solved

--- 8kyuproblems.js
+++ 8kyuproblems.js
@@ -657,3 +657,18 @@ function disemvowel(str) {
   }
   return newArr.join('')
 }
+/*
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level*/ 
+function printArray(array){
+  //show me the code!
+
+  return array.join(',');
+}
